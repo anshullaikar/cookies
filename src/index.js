94b510fee6d6b9589fe707d60698bd62112abaf9
Anshul Laikar <anshullaikar@gmail.com>
2021-04-08 00:50:02 +0530

    Tailwind redo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter, HashRouter} from "react-router-dom";
+import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-      <App /> 
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
