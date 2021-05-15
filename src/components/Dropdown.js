b4bfe78159396240b76d46eba3f5dabb4a6c113e
anshul <anshullaikar@gmail.com>
2021-05-16 02:54:35 +0530

    fixed routes... for real

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const Dropdown = () => {
     return (
         <div className="md:hidden font-mono grid grid-rows-4 text-center items-center dropdown">
-            <Link to="#hero" className="p-4">
+            <Link to="/cookies/" className="p-4">
                 Home
             </Link>
             <Link to="/cookies/about" className="p-4">
                 About
             </Link>
-            <a href="#items" className="p-4">
+            <a href="/cookies/#items" className="p-4">
                 Order Now
             </a>
         </div>
